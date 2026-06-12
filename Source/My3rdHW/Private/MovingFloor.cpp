0dbcff51a853e2f0155b8a852512fc76af980895
AntiFreeze81 <wnsgud3590@naver.com>
2026-06-12 15:37:20 +0900

    feat: MovingFloor 클래스 추가

---
+++ Source/My3rdHW/Private/MovingFloor.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "My3rdHW/Public/MovingFloor.h"
+
+
+// Sets default values
+AMovingFloor::AMovingFloor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMovingFloor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMovingFloor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/My3rdHW/Public/MovingFloor.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingFloor.generated.h"
+
+UCLASS()
+class MY3RDHW_API AMovingFloor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMovingFloor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
