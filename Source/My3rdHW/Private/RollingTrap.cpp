5b43a9f708b9b2de0ca50da35bbe42af86d9b7cc
AntiFreeze81 <wnsgud3590@naver.com>
2026-06-15 11:10:07 +0900

    feat: add rolling-trap class

---
+++ Source/My3rdHW/Private/RollingTrap.cpp
@@ -0,0 +1,32 @@
+﻿#include "RollingTrap.h"
+
+ARollingTrap::ARollingTrap()
+{
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+	
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComp");
+	StaticMeshComp->SetupAttachment(SceneRoot);
+	
+	StaticMeshCompLeft = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshCompLeft");
+	StaticMeshCompLeft->SetupAttachment(StaticMeshComp);
+	
+	StaticMeshCompRight = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshCompRight");
+	StaticMeshCompRight->SetupAttachment(StaticMeshComp);
+	
+	
+	
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ARollingTrap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ARollingTrap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/My3rdHW/Public/RollingTrap.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RollingTrap.generated.h"
+
+UCLASS()
+class MY3RDHW_API ARollingTrap : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ARollingTrap();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MovingFloor|Components")
+	USceneComponent* SceneRoot;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MovingFloor|Components")
+	UStaticMeshComponent* StaticMeshComp;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MovingFloor|Components")
+	UStaticMeshComponent* StaticMeshCompLeft;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MovingFloor|Components")
+	UStaticMeshComponent* StaticMeshCompRight;
+	
+public:
+	virtual void Tick(float DeltaTime) override;
+};
