dcaabcaf6b40d7bd217e186b0557f1077cb92597
AntiFreeze81 <wnsgud3590@naver.com>
2026-06-16 16:21:46 +0900

    feat: falling-ball-spawn-mamager 클래스 생성

---
+++ Source/My3rdHW/Private/FallingBallSpawnManager.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FallingBallSpawnManager.h"
+
+
+// Sets default values
+AFallingBallSpawnManager::AFallingBallSpawnManager()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFallingBallSpawnManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFallingBallSpawnManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
